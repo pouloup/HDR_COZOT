0c16dd0200bbfd46519fffc8a3c391daac4b8ce1
pouloup <paul.ledenn@wanadoo.fr>
2015-10-13 14:36:09 +0200

    ajout de l'algo gsolve.cpp + arborescence du dépôt  Modifications qui seront validées : 	nouveau :   include/gsolve.h 	nouveau :   src/gsolve.cpp

---
+++ src/gsolve.cpp
@@ -0,0 +1,13 @@
+#include <gsolve.h>
+
+int Zmin = 0;
+int Zmax = 255;
+
+/*
+ * Z : valeur du pixel i correspondant dans l'image j;
+ * 
+ */
+
+void gsolve(int Z(int i, int j), double B, double l, double w(int px), double *g, double *lE){
+	
+}
