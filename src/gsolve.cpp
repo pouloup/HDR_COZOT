be2fc3985af122f3a3d6ece81eff0d4732047844
pouloup <paul.ledenn@wanadoo.fr>
2015-10-13 14:59:53 +0200

    Modifications qui seront validées : 	modifié :   src/gsolve.cpp

--- src/gsolve.cpp
+++ src/gsolve.cpp
@@ -1,13 +1,19 @@
 #include <gsolve.h>
+#inclde <opencv/opencv2>
 
 int Zmin = 0;
 int Zmax = 255;
 
 /*
- * Z : valeur du pixel i correspondant dans l'image j;
- * 
+ * Z() : valeur du pixel i correspondant dans l'image j;
+ * B : log shutter speed de l'image j;
+ * l : lambda, constante qui détermine l'onctuosité de la courbe;
+ * w() : fonction d'influence pour le pixel de valeur val;
+ * g : log de l'exposition correspondant au pixel de valeur val;
+ * lE : log film irradiance au pixel de location i.
  */
 
-void gsolve(int Z(int i, int j), double B, double l, double w(int px), double *g, double *lE){
+void gsolve(int Z(int i, int j), double B, double l, double w(int val), double *g, double *lE){
+	int n = 256;
 	
 }
