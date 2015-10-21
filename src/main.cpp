62723e91748d5c80be50d0529b0ce903e0f8f0dd
pouloup <paul.ledenn@wanadoo.fr>
2015-10-21 17:06:46 +0200

    Structure complete

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,8 @@
 using namespace cv;
 using namespace std;
 
-int main(int, char**argv){
-    
-    /*
-     * PART 1 : Load Images
-     * Pictures (.png) & Exposure time
-     */
-	vector<Mat> images;
-    vector<float> times;
-    
+void load_im_expo(vector<Mat>& images, vector<float>& times){
+
     String path = std::string("../");
     ifstream list_file((path + "list_photos.txt").c_str());
 	
@@ -34,7 +27,7 @@ int main(int, char**argv){
         FILE *fp = fopen(std::string(path+name).c_str(), "rb");
         if (!fp) { 
             printf("Can't open file.\n"); 
-            return -1; 
+            return ; 
         }
         
         fseek(fp, 0, SEEK_END);
@@ -45,7 +38,7 @@ int main(int, char**argv){
         if (fread(buf, 1, fsize, fp) != fsize) {
             printf("Can't read file.\n");
             delete[] buf;
-            return -2;
+            return ;
         }
         
         fclose(fp);
@@ -62,11 +55,51 @@ int main(int, char**argv){
 	list_file.close();
     
     if(images.empty() || times.empty())
-        cerr << "Loading failure" << endl;
+        cerr << "Loading failure" << endl;    
+}
+
+void estimate_camera_response(Mat& response, vector<Mat>& images, vector<float>& times){
     
-    /*
-     * END PART 1
-     */
+    Ptr<CalibrateDebevec> calibrate = createCalibrateDebevec();
+    calibrate->process(images, response, times);
+}
+
+void construct_hdr(Mat& hdr, Mat& response, vector<Mat>& images, vector<float>& times){
+    
+    Ptr<MergeDebevec> merge_debevec = createMergeDebevec();
+    merge_debevec->process(images, hdr, times, response);
+}
+
+void construct_tonemap(Mat& ldr, Mat& hdr){
+    
+    Ptr<TonemapDurand> tonemap = createTonemapDurand(2.2f);
+    tonemap->process(hdr, ldr);
+}
+
+void construct_fusion(Mat& fusion, vector<Mat>& images){
+
+    Ptr<MergeMertens> merge_mertens = createMergeMertens();
+    merge_mertens->process(images, fusion);
+}
+
+int main(int, char**argv){
+    
+    vector<Mat> images;
+    vector<float> times;
+    Mat response, hdr, ldr, fusion;
+    
+    load_im_expo(images, times);
+    estimate_camera_response(response, images, times);
+    construct_hdr(hdr, response, images, times);
+    construct_tonemap(ldr, hdr);
+    construct_fusion(fusion, images);
+    
+    imwrite("fusion.png", fusion * 255);
+    cout << "fusion write success." << endl;
+    imwrite("ldr.png", ldr * 255);
+    cout << "ldr write success." << endl;
+    imwrite("hdr.hdr", hdr);
+    cout << "hdr write success." << endl;
 	
     return 0;
 }
